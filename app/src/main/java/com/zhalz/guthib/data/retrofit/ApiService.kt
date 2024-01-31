0d71c69fcf18778fdf6321dfa408f81b188cbc41
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-01-31 15:56:41 +0700

    [CREATE] Header for limit raise

--- app/src/main/java/com/zhalz/guthib/data/retrofit/ApiService.kt
+++ app/src/main/java/com/zhalz/guthib/data/retrofit/ApiService.kt
@@ -4,18 +4,22 @@ import com.zhalz.guthib.data.response.UserData
 import com.zhalz.guthib.data.response.UserResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("search/users")
+    @Headers("Authorization: token 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")
     fun searchUser( @Query(value = "q") query: String) : Call<UserResponse>
 
     @GET("users/{username}/followers")
+    @Headers("Authorization: token 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")
     fun getFollowers( @Path("username") username: String ) : Call<List<UserData>>
 
     @GET("users/{username}/following")
+    @Headers("Authorization: token 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")
     fun getFollowing( @Path("username") username: String ) : Call<List<UserData>>
 
 }

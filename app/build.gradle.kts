437f6f2e6f02988519f5f554bd9e013ba8d9e1a6
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-02-04 20:09:30 +0700

    [CREATE] Hidden API KEY

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -16,6 +18,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val properties = Properties()
+        properties.load(project.rootProject.file("local.properties").inputStream())
+        buildConfigField("String", "GITHUB_API_KEY", properties.getProperty("GITHUB_API_KEY"))
     }
 
     buildTypes {
@@ -36,6 +42,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
--- app/src/main/java/com/zhalz/guthib/data/retrofit/ApiService.kt
+++ app/src/main/java/com/zhalz/guthib/data/retrofit/ApiService.kt
@@ -4,27 +4,33 @@ import com.zhalz.guthib.data.response.DetailUser
 import com.zhalz.guthib.data.response.UserData
 import com.zhalz.guthib.data.response.UserResponse
 import retrofit2.Call
+import com.zhalz.guthib.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Query
 
+
 interface ApiService {
 
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: $apiKey")
     fun searchUser( @Query(value = "q") query: String) : Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: $apiKey")
     fun getDetail( @Path("username") username: String ) : Call<DetailUser>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: $apiKey")
     fun getFollowers( @Path("username") username: String ) : Call<List<UserData>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: $apiKey")
     fun getFollowing( @Path("username") username: String ) : Call<List<UserData>>
+
+    companion object {
+        const val apiKey = BuildConfig.GITHUB_API_KEY
+    }
 
 }

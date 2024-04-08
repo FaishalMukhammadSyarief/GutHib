5ea22cbc03388eb83e102a326f05052b411a4379
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-04-08 20:35:18 +0700

    [CREATE] Room Database dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.activity:activity:1.8.2")
+    implementation("androidx.activity:activity-ktx:1.8.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -91,5 +91,10 @@ dependencies {
 
     /*  ==  LIVE DATA  ==  */
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+
+    /*  ==  ROOM DATABASE  ==  */
+    //noinspection KaptUsageInsteadOfKsp
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
 
 }

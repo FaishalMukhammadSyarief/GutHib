10c9b23899dc5d18e9c90ee2d6818f31c4baa655
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-04-11 06:01:40 +0700

    [CREATE] Toolbar in SettingActivity

--- app/src/main/java/com/zhalz/guthib/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/zhalz/guthib/ui/setting/SettingActivity.kt
@@ -45,6 +45,9 @@ class SettingActivity : AppCompatActivity() {
     }
 
     private fun initUI() {
+        /** == TOOLBAR == **/
+        binding.toolbar.setNavigationOnClickListener { finish() }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
